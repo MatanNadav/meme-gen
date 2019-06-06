7c23c44ac8e3b7ef785b63ad196f94c9ed4c4fc8
Matan Nadav <Smatannadav@gmail.com>
2019-06-06 15:11:28 +0300

    img link success

--- js/main-controller.js
+++ js/main-controller.js
@@ -11,7 +11,7 @@ function onInitMain() {
 function createImgContainer(size) {
     var grid = document.querySelector('.grid-container');
     for (var i = 0; i < size; i++) {
-        grid.innerHTML +=  `<div class = "box" onmousemove="onDisplayInfo(this)"></div>`
+        grid.innerHTML +=  `<div class = "box hvr-grow-shadow" onmousemove="onDisplayInfo(this)"></div>`
     }
 }
 function renderImage(imgs) {
@@ -19,16 +19,16 @@ function renderImage(imgs) {
     imgs.forEach(img => {
         var imgBox = document.querySelectorAll('.box');
         var keywords = img.keywords.join(',')
-        imgBox[idx].innerHTML = `<p class="meme-info">${keywords}</p> <img class="images img-${img.id}" id="${img.id}" src="${img.imgUrl}" 
-        alt="" onclick="onImgClick(this)"> `
+        imgBox[idx].innerHTML = `<a href="generator.html"><p class="meme-info">${keywords}</p> <img class="images img-${img.id}" id="${img.id}" src="${img.imgUrl}" 
+        alt="" onclick="onImgClick(this)"></a>`
         idx++;
     });
 }
 
 function onImgClick(elImg) {
     
-    window.location = 'generator.html';
     drawImgOnCanvas(elImg);
+    window.location = 'generator.html';
     // console.log(img);
 
 }
