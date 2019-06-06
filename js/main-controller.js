71c9ee4b30da1fe1e8bba5eec71ab6c5af9db318
Matan Nadav <Smatannadav@gmail.com>
2019-06-06 11:59:36 +0300

    commit

--- js/main-controller.js
+++ js/main-controller.js
@@ -2,12 +2,9 @@
 
 function onInit() {
     var imgs = createImages();
-<<<<<<< HEAD
     // renderImage(imgs)
-=======
     
    
->>>>>>> 2bd579a8e1a17c5661004c42f66978a80ff5b303
     console.log(imgs);
 
 }
