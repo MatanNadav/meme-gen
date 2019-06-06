3636baf8b52e46af513f0b5e245840dd48e0db74
Matan Nadav <Smatannadav@gmail.com>
2019-06-06 13:14:29 +0300

    get img by id

--- js/main-controller.js
+++ js/main-controller.js
@@ -13,7 +13,14 @@ function renderImage(imgs) {
     var idx = 0;
     imgs.forEach( img =>  {
         var imgBox = document.querySelectorAll('.box');
-        imgBox[idx].innerHTML = `<img class="images img-${img.id}" src="${img.imgUrl}" alt="">`
+        imgBox[idx].innerHTML = `<img class="images img-${img.id}" id="${img.id}" src="${img.imgUrl}" alt="" onclick="onImgClick(this)">`
         idx++;
     } );
+}
+
+function onImgClick(elImg) {
+    debugger
+    var img = getImgById(+elImg.id);
+    console.log(img);
+    
 }
