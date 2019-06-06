351f1c2c263007fc667081209dc1cf3ce791010d
Matan Nadav <Smatannadav@gmail.com>
2019-06-06 10:37:54 +0300

    create images

--- js/controller.js
+++ js/controller.js
@@ -2,14 +2,8 @@
 
 function init() {
     var imgs = createImages()
-    id = [
-        genId(),
-        genId(),
-        genId(),
-        genId(),
-    ]
+   
     console.log(imgs);
-    console.log(id);
     
     
 }
