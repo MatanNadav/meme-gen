ff0b377d28b36ced1bd86f6c95eb806d68812c0c
Itay Azulay <itay.responder@gmail.com>
2019-06-07 20:00:04 +0300

    img-size

--- js/generator-controller.js
+++ js/generator-controller.js
@@ -8,47 +8,49 @@ let gImg = new Image();
 function onInitGenerator() {
     canvas = document.querySelector('.meme-canvas')
     ctx = canvas.getContext('2d');
-    ctx.textAlign = 'center'
-    ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'black';
-    ctx.font = '40px impact';
     drawImgOnCanvas();
-    gPos.x = canvas.width / 2;
-    gPos.y = 100;
 }
 
 function drawImgOnCanvas() {
     let imgFromStorage = getValue('meme');
     gImg.src = imgFromStorage.imgUrl;
     gImg.onload = function () {
+        canvas.width = gImg.naturalWidth;
+        canvas.height = gImg.naturalHeight;
+        ctx.textAlign = 'center'
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'black';
+        ctx.font = '40px impact';
+        gPos.x = canvas.width / 2;
+        gPos.y = 100;
         ctx.drawImage(gImg, 0, 0, canvas.width, canvas.height);
     }
 }
 
 function drawTextOnCanvas() {
     let text = document.querySelector('.text').value;
-    ctx.drawImage(gImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(gImg, 0, 0, gImg.naturalWidth, gImg.naturalHeight);
     ctx.strokeText(text, gPos.x, gPos.y);
     ctx.fillText(text, gPos.x, gPos.y);
 }
 
 function textAlign(alignText) {
-    ctx.drawImage(gImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(gImg, 0, 0, gImg.naturalWidth, gImg.naturalHeight);
     let text = document.querySelector('.text').value;
     ctx.textAlign = alignText;
     if (alignText === 'align-left') {
-        gPos.x = 100;
+        gPos.x = 10;
     } else if (alignText === 'align-center') {
-        gPos.x = canvas.width / 2;
+        gPos.x = gImg.naturalWidth / 2;
     } else {
-        gPos.x = canvas.width - 40;
+        gPos.x = gImg.naturalWidth - 40;
     }
     ctx.strokeText(text, gPos.x, gPos.y);
     ctx.fillText(text, gPos.x, gPos.y);
 }
 
 function fontSize(fontSize) {
-    ctx.drawImage(gImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(gImg, 0, 0, gImg.naturalWidth, gImg.naturalHeight);
     let text = document.querySelector('.text').value;
     if (fontSize === 'increese-font-size') {
         ctx.font = ctx.font.replace(/\d+px/, (parseInt(ctx.font.match(/\d+px/)) + 2) + 'px');
@@ -60,8 +62,7 @@ function fontSize(fontSize) {
 }
 
 function changeColor(color) {
-    console.log(color);
-    ctx.drawImage(gImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(gImg, 0, 0, gImg.naturalWidth, gImg.naturalHeight);
     ctx.fillStyle = color;
     let text = document.querySelector('.text').value;
     ctx.strokeText(text, gPos.x, gPos.y);
