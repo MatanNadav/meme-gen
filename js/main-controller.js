6a7e4a6fc705fdc2478f68e5efa1086ddba46895
Matan Nadav <Smatannadav@gmail.com>
2019-06-08 15:31:09 +0300

    search dropdown

--- js/main-controller.js
+++ js/main-controller.js
@@ -5,7 +5,8 @@
 function onInitMain() {
     let imgs = createImages();
     createImgContainer(imgs.length)
-    renderImage(imgs)
+    renderImage(imgs);
+    renderPopularSearch();
     console.log('database', imgs);
 
 }
@@ -30,27 +31,34 @@ function renderImage(imgs) {
 function renderPopularSearch() {
     let strSearches = '';
     let searches = getPopularSearchesArray()
-    var idx = 0;
+    let idx = 0;
+    let searchOptions = document.querySelectorAll('.search-option')
     while (idx < 6) {
         if (!searches[idx]) break;
         switch (idx) {
             case 0:
                 strSearches += `<h1 class="popular popular-h1" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h1>`
+                searchOptions[idx].value = searches[idx][0];
                 break;
             case 1:
                 strSearches += `<h2 class="popular popular-h2" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h2>`
+                searchOptions[idx].value = searches[idx][0];
                 break;
             case 2:
                 strSearches += `<h3 class="popular popular-h3" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h3>`
+                searchOptions[idx].value = searches[idx][0];
                 break;
             case 3:
                 strSearches += `<h4 class="popular popular-h4" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h4>`
+                searchOptions[idx].value = searches[idx][0];
                 break;
             case 4:
                 strSearches += `<h5 class="popular popular-h5" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h5>`
+                searchOptions[idx].value = searches[idx][0];
                 break;
             case 5:
                 strSearches += `<h6 class="popular popular-h6" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h6>`
+                searchOptions[idx].value = searches[idx][0];
                 break;
         }
         idx++;
@@ -61,6 +69,8 @@ function renderPopularSearch() {
 
 
 function onSearchMeme(inputVal) {
+    console.log('here');
+    
     var searchedImgs = getSearchedMemes(inputVal);
     if (searchedImgs[0]) clearGrid()
     createImgContainer(searchedImgs.length)
