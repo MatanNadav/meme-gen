ddeaf3e818a70d83d69bc81572f96d96e5ed3b76
Matan Nadav <Smatannadav@gmail.com>
2019-06-08 14:48:37 +0300

    popular searches

--- js/main-controller.js
+++ js/main-controller.js
@@ -7,13 +7,13 @@ function onInitMain() {
     createImgContainer(imgs.length)
     renderImage(imgs)
     console.log('database', imgs);
-    
+
 }
 
 function createImgContainer(size) {
     let grid = document.querySelector('.grid-container');
     for (let i = 0; i < size; i++) {
-        grid.innerHTML +=  `<div class = "box hvr-grow-shadow"></div>`
+        grid.innerHTML += `<div class = "box hvr-grow-shadow"></div>`
     }
 }
 
@@ -27,13 +27,46 @@ function renderImage(imgs) {
     });
 }
 
+function renderPopularSearch() {
+    let strSearches = '';
+    let searches = getPopularSearchesArray()
+    var idx = 0;
+    while (idx < 6) {
+        if (!searches[idx]) break;
+        switch (idx) {
+            case 0:
+                strSearches += `<h1 class="popular popular-h1" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h1>`
+                break;
+            case 1:
+                strSearches += `<h2 class="popular popular-h2" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h2>`
+                break;
+            case 2:
+                strSearches += `<h3 class="popular popular-h3" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h3>`
+                break;
+            case 3:
+                strSearches += `<h4 class="popular popular-h4" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h4>`
+                break;
+            case 4:
+                strSearches += `<h5 class="popular popular-h5" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h5>`
+                break;
+            case 5:
+                strSearches += `<h6 class="popular popular-h6" style="color: firebrick" onclick="onSearchMeme(this.innerText)">${searches[idx][0]}</h6>`
+                break;
+        }
+        idx++;
+    }
+    let popularContainer = document.querySelector('.popular-container');
+    popularContainer.innerHTML = strSearches;
+}
+
 
 function onSearchMeme(inputVal) {
-    popularSearched(inputVal)
     var searchedImgs = getSearchedMemes(inputVal);
-    if(searchedImgs[0]) clearGrid()
+    if (searchedImgs[0]) clearGrid()
     createImgContainer(searchedImgs.length)
-    renderImage(searchedImgs);    
+    renderImage(searchedImgs);
+    popularSearched(inputVal);
+    renderPopularSearch();
 }
 
 function clearGrid() {
@@ -50,6 +83,6 @@ function onImgClick(imgId) {
 }
 
 function popularSearched(inputVal) {
-    if(!inputVal) return
+    if (!inputVal) return
     setPopularSearches(inputVal)
 }
--- js/main-service.js
+++ js/main-service.js
@@ -3,7 +3,7 @@
 let gImgs;
 let gImagesToRemove = [];
 let gId = 1;
-let gPopularSearch = {};
+let gPopularSearch;
 
 
 function createImages() {
@@ -17,28 +17,28 @@ function createImages() {
         createImage('imgs/img6.jpg', ['tired', 'dog']),
         createImage('imgs/003.jpg', ['trump', 'orange', 'vain', 'angry']),
         createImage('imgs/004.jpg', ['dog', 'cute', 'love', 'sweet']),
-        createImage('imgs/005.jpg', ['dog', 'baby','sleep', 'cute','smile','bed']),
+        createImage('imgs/005.jpg', ['dog', 'baby', 'sleep', 'cute', 'smile', 'bed']),
         createImage('imgs/006.jpg', ['cat', 'sleep', 'computer']),
         createImage('imgs/12.jpg', ['blaming', 'finger']),
         createImage('imgs/Ancient-Aliens.jpg', ['if', 'history', 'aliens', 'let me explain']),
         createImage('imgs/drevil.jpg', ['evil', 'quotes', 'fake', 'powers']),
-        createImage('imgs/img2.jpg', ['africa', 'baby','dance','fun','naked']),
-        createImage('imgs/img5.jpg', ['baby', 'surprise', 'funny','cute']),
-        createImage('imgs/leo.jpg', ['leo dicaprio', 'salute', 'cheers','fancy']),
+        createImage('imgs/img2.jpg', ['africa', 'baby', 'dance', 'fun', 'naked']),
+        createImage('imgs/img5.jpg', ['baby', 'surprise', 'funny', 'cute']),
+        createImage('imgs/leo.jpg', ['leo dicaprio', 'salute', 'cheers', 'fancy']),
         createImage('imgs/meme1.jpg', ['matrix', 'what if', 'serious']),
-        createImage('imgs/meme2.jpg', ['one does not', 'game thrones', 'sean','explain']),
+        createImage('imgs/meme2.jpg', ['one does not', 'game thrones', 'sean', 'explain']),
         createImage('imgs/meme3.jpg', ['oprah', 'you get', 'shout']),
-        createImage('imgs/patrick.jpg', ['patrick', 'holding', 'laugh','cry']),
-        createImage('imgs/putin.jpg', ['putin', 'two', 'strong','russia']),
-        createImage('imgs/meme4.jpg', ['toy story', 'two', 'buzz','cowboy','stars','look']),
-        createImage('imgs/russian.jpg', ['russian', 'badass', 'sexy','beer']),
-        createImage('imgs/arm-wrestle.png', ['strong', 'muscle', 'black','white','arm wrestle']),
-        createImage('imgs/meme5.jpg', ['thinking', 'know best', 'black','use mind']),
-        createImage('imgs/grandma.jpg', ['grandma', 'cant see', 'glasses','backwords','understand']),
+        createImage('imgs/patrick.jpg', ['patrick', 'holding', 'laugh', 'cry']),
+        createImage('imgs/putin.jpg', ['putin', 'two', 'strong', 'russia']),
+        createImage('imgs/meme4.jpg', ['toy story', 'two', 'buzz', 'cowboy', 'stars', 'look']),
+        createImage('imgs/russian.jpg', ['russian', 'badass', 'sexy', 'beer']),
+        createImage('imgs/arm-wrestle.png', ['strong', 'muscle', 'black', 'white', 'arm wrestle']),
+        createImage('imgs/meme5.jpg', ['thinking', 'know best', 'black', 'use mind']),
+        createImage('imgs/grandma.jpg', ['grandma', 'cant see', 'glasses', 'backwords', 'understand']),
         createImage('imgs/office.jpg', ['office', 'coffee', 'spandex']),
-        createImage('imgs/grylls.jpg', ['bear', 'grylls', 'improvise','adapt', 'overcome']),
-        createImage('imgs/ohno.jpg', ['stress', 'what to push', 'sweat','pressure', 'bomb']),
-        createImage('imgs/drake.jpg', ['yes', 'no', 'oh','feel good', 'dance']),
+        createImage('imgs/grylls.jpg', ['bear', 'grylls', 'improvise', 'adapt', 'overcome']),
+        createImage('imgs/ohno.jpg', ['stress', 'what to push', 'sweat', 'pressure', 'bomb']),
+        createImage('imgs/drake.jpg', ['yes', 'no', 'oh', 'feel good', 'dance']),
     ]
     gImgs = imgs;
     return imgs;
@@ -54,38 +54,43 @@ function createImage(url, words) {
 }
 
 function getImgById(id) {
-    return gImgs.find( img => {
-            return img.id === id;
-        })
+    return gImgs.find(img => {
+        return img.id === id;
+    })
 }
 
 function getSearchedMemes(searchStr) {
     return gImgs.filter(img => {
         let keywordStr = img.keywords.join(',');
-        if((keywordStr.indexOf(searchStr)) > -1) {
+        if ((keywordStr.indexOf(searchStr)) > -1) {
             return img
-        } else {
-
         }
     })
 }
 
+
 function setPopularSearches(inputVal) {
     let str = inputVal;
-    if(gPopularSearch[str]) {
+    gPopularSearch = getValue('popularSearch')
+    if (gPopularSearch[str]) {
         gPopularSearch[str]++;
     } else {
         gPopularSearch[str] = 1;
     }
     saveValue('popularSearch', gPopularSearch)
     console.log(gPopularSearch);
-    
 }
+
+function getPopularSearchesArray() {
+    var searches = getSortedArrayFromObject();
+    return searches;
+}
+
 
 function saveValue(key, value) {
     saveToStorage(key, value);
 }
 
 function getValue(key) {
-   return loadFromStorage(key);
+    return loadFromStorage(key);
 }
